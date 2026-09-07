8a1e77d6ac82fcf43f4e271ddf6a666139fd3cb1
Daniel Esteban Palma Igor <daniel.palma.i@ug.uchile.cl>
2026-09-07 16:58:45 -0300

    added multiple files support

--- strucTFactor/predictTFwithStrucTFactor.py
+++ strucTFactor/predictTFwithStrucTFactor.py
@@ -2,6 +2,7 @@
 # you can choose your own pre-trained model or take the default model
 # run it via: python predictTFwithStrucTFactor.py -i example.pdb -o output.csv -ckpt strucTFactor_model.pt -g cuda:0
 import argparse
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
 
 import torch
@@ -18,46 +19,50 @@ def argument_parser():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-g", "--gpu", required=False, default="cpu", help="Specify gpu"
+        "-d",
+        "--device",
+        required=False,
+        default="cpu",
+        help="Specify device (Default: cpu)",
     )
     parser.add_argument(
-        "-ckpt",
+        "-c",
         "--checkpoint",
         required=False,
         default=files("strucTFactor").joinpath("strucTFactor_model.pt"),
-        help="Checkpoint file needed!",
+        help="Checkpoint file",
     )
-    parser.add_argument(
-        "-i", "--pdb_file", required=False, default="example.pdb", help="PDB file"
-    )
+    parser.add_argument("-i", "--input", required=True, nargs="+", help="Input files")
     parser.add_argument(
         "-f",
         "--format",
         choices=["pdb", "mmcif"],
         default="pdb",
-        help="input format",
+        help="input format (Default: pdb)",
     )
+    parser.add_argument("-o", "--output", required=True, help="Output file")
     parser.add_argument(
-        "-o", "--output", required=False, default="output.csv", help="Output file"
+        "-t",
+        "--threads",
+        required=False,
+        default=1,
+        type=int,
+        help="Number of threads (Default: 1)",
     )
     return parser
 
 
-def get_spatial_string(PDB_filename, seq_id, file_format="pdb"):
+def get_spatial_string(path, seq_id, file_format="pdb"):
     if file_format == "pdb":
         parser = PDBParser()
     elif file_format == "mmcif":
         parser = MMCIFParser()
     else:
         raise ValueError(f"Invalid file_format: {file_format}")
-    try:
-        structure = parser.get_structure(seq_id, PDB_filename)
-        model = structure[0]
-        dssp = DSSP(model, PDB_filename, dssp="dssp")
-        dssp_keys = list(dssp.keys())
-    except Exception as e:
-        print(f"Problem with id: {seq_id}: {e}")
-        return None
+    structure = parser.get_structure(seq_id, path)
+    model = structure[0]
+    dssp = DSSP(model, path, dssp="dssp")
+    dssp_keys = list(dssp.keys())
 
     seq = []
     spatial_str = []
@@ -79,47 +84,61 @@ def get_spatial_string(PDB_filename, seq_id, file_format="pdb"):
 def main():
     parser = argument_parser()
     args = parser.parse_args()
-    device = torch.device(args.gpu)
+    device = torch.device(args.device)
     checkpt_file = args.checkpoint
-    pdb_file = args.pdb_file
+    input_files = args.input
     file_format = args.format
-    output_file = args.output
+    output_path = args.output
+    threads = args.threads
     # Load the model
     model = DeepTFactor(
         1,
         out_features=[1],
     )
     model = model.to(device)
-    cutoff = 0.5
-
-    # run DSSP to get spatial information
-    protein_seqs, spatial_seqs = get_spatial_string(
-        pdb_file, "test", file_format=file_format
-    )
-    seq_ids = Path(pdb_file).with_suffix("").name
-    protein_seqs += "_" * (1000 - len(protein_seqs))  # zero-padding
-    proteinDataset = EnzymeDataset_spatial(
-        [protein_seqs], torch.zeros([1, 1]), [spatial_seqs], 1
-    )
-
-    test_loader = DataLoader(proteinDataset, batch_size=1, shuffle=False)
     ckpt = torch.load(f"{checkpt_file}", map_location=device)
     model.load_state_dict(ckpt["model"])
 
-    y_pred = torch.zeros([1, 1])
-    with torch.no_grad():
+    cutoff = 0.5
+
+    # run DSSP to get spatial information
+    future_dict = {}
+    executor = ProcessPoolExecutor(max_workers=threads)
+    for file in input_files:
+        seq_id = Path(file).with_suffix("").name
+        future = executor.submit(
+            get_spatial_string, file, seq_id, file_format=file_format
+        )
+        future_dict[future] = seq_id
+
+    with torch.no_grad(), open(output_path, "w") as out_file:
         model.eval()
-        cnt = 0
-        for x, _ in test_loader:
-            x = x.type(torch.FloatTensor)
-            output = model(x.to(device))
-            prediction = output.cpu()
-    if prediction.item() > cutoff:
-        print(f"Prediction for {seq_ids}: Transcription Factor")
-    else:
-        print(f"Prediction for {seq_ids}: Not a Transcription Factor")
-    with open(output_file, "w") as f:
-        f.write(f"{seq_ids}\t{prediction.item()}\n")
+        for future in as_completed(future_dict):
+            seq_id = future_dict[future]
+            try:
+                protein_seqs, spatial_seqs = future.result()
+            except Exception as exc:
+                print(f"Error: {exc}")
+                continue
+            length = len(protein_seqs)
+            if length > 1000:
+                print(f"Warning: {seq_id} is too long ({length})")
+                continue
+            protein_seqs += "_" * (1000 - length)  # zero-padding
+            proteinDataset = EnzymeDataset_spatial(
+                [protein_seqs], torch.zeros([1, 1]), [spatial_seqs], 1
+            )
+
+            test_loader = DataLoader(proteinDataset, batch_size=1, shuffle=False)
+            for x, _ in test_loader:
+                x = x.type(torch.FloatTensor)
+                output = model(x.to(device))
+                prediction = output.cpu()
+            if prediction.item() > cutoff:
+                print(f"Prediction for {seq_id}: Transcription Factor")
+            else:
+                print(f"Prediction for {seq_id}: Not a Transcription Factor")
+            out_file.write(f"{seq_id}\t{prediction.item()}\n")
 
 
 if __name__ == "__main__":
